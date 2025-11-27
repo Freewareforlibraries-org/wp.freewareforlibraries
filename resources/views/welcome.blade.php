6f304092452c4f5745c3a96e32143ef07e83599f
Adam Branscum <93352907+adambranscum@users.noreply.github.com>
2025-11-26 22:47:23 -0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Community Print</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 <body class="bg-gray-50">
     <div class="min-h-screen flex flex-col">
